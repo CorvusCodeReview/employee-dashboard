45bc86a1eb14cc33c08e038b2bf06c61ad37c85b
Corvus Code Review <67117088+CorvusCodeReview@users.noreply.github.com>
2026-04-17 16:43:21 +0530

    Get things started for Dashboard

---
+++ app.py
@@ -0,0 +1,251 @@
+from flask import Flask, render_template, redirect, url_for, request, send_file
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+import pandas as pd
+
+app = Flask(__name__)
+app.secret_key = "your_secret_key_here"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+# ------------------ MODELS ------------------
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    role = db.Column(db.String(50))
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    suggested_time = db.Column(db.String(50))
+
+
+class Report(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    date = db.Column(db.String(50))
+
+
+class ReportTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    report_id = db.Column(db.Integer)
+    task_id = db.Column(db.Integer)
+    actual_time = db.Column(db.String(50))
+    notes = db.Column(db.String(500))
+
+
+# ------------------ LOGIN ------------------
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/')
+@login_required
+def dashboard():
+    return f"Hello {current_user.username}! Role: {current_user.role}"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(
+            username=request.form['username'],
+            password=request.form['password']
+        ).first()
+
+        if user:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+
+        return "Invalid credentials"
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+# ------------------ SUBMIT REPORT ------------------
+
+@app.route('/submit_report', methods=['GET', 'POST'])
+@login_required
+def submit_report():
+
+    today = str(date.today())
+    existing_report = Report.query.filter_by(user_id=current_user.id, date=today).first()
+
+    if request.method == 'POST':
+        task_ids = request.form.getlist('task')
+        times = request.form.getlist('time')
+        notes_list = request.form.getlist('notes')
+
+        if existing_report:
+            ReportTask.query.filter_by(report_id=existing_report.id).delete()
+
+            for i in range(len(task_ids)):
+                db.session.add(ReportTask(
+                    report_id=existing_report.id,
+                    task_id=task_ids[i],
+                    actual_time=times[i],
+                    notes=notes_list[i]
+                ))
+
+            db.session.commit()
+            return "Report updated successfully!"
+
+        else:
+            report = Report(user_id=current_user.id, date=today)
+            db.session.add(report)
+            db.session.commit()
+
+            for i in range(len(task_ids)):
+                db.session.add(ReportTask(
+                    report_id=report.id,
+                    task_id=task_ids[i],
+                    actual_time=times[i],
+                    notes=notes_list[i]
+                ))
+
+            db.session.commit()
+            return "Report submitted successfully!"
+
+    tasks = Task.query.all()
+
+    existing_tasks = []
+    if existing_report:
+        existing_tasks = ReportTask.query.filter_by(report_id=existing_report.id).all()
+
+    return render_template('submit_report.html', tasks=tasks, existing_tasks=existing_tasks)
+
+
+# ------------------ MY REPORTS ------------------
+
+@app.route('/my_reports')
+@login_required
+def my_reports():
+    reports = Report.query.filter_by(user_id=current_user.id).all()
+    return render_template('my_reports.html', reports=reports)
+
+
+# ------------------ ALL REPORTS ------------------
+
+@app.route('/all_reports')
+@login_required
+def all_reports():
+
+    if current_user.role not in ['manager', 'senior']:
+        return "Access Denied"
+
+    selected_user = request.args.get('user_id')
+    selected_date = request.args.get('date')
+
+    query = Report.query
+
+    if selected_user:
+        query = query.filter_by(user_id=selected_user)
+
+    if selected_date:
+        query = query.filter_by(date=selected_date)
+
+    reports = query.all()
+    users = User.query.all()
+
+    return render_template(
+        'all_reports.html',
+        reports=reports,
+        users=users,
+        selected_user=selected_user,
+        selected_date=selected_date
+    )
+
+
+# ------------------ REPORT DETAILS ------------------
+
+@app.route('/report/<int:report_id>')
+@login_required
+def report_details(report_id):
+
+    report = Report.query.get(report_id)
+
+    tasks = ReportTask.query.filter_by(report_id=report.id).all()
+    task_names = {task.id: task.name for task in Task.query.all()}
+    user = User.query.get(report.user_id)
+
+    return render_template(
+        'report_details.html',
+        report=report,
+        tasks=tasks,
+        task_names=task_names,
+        user=user
+    )
+
+
+# ------------------ EXPORT EXCEL ------------------
+
+@app.route('/export')
+@login_required
+def export_excel():
+
+    if current_user.role not in ['manager', 'senior']:
+        return "Access Denied"
+
+    selected_user = request.args.get('user_id')
+    selected_date = request.args.get('date')
+
+    query = Report.query
+
+    if selected_user:
+        query = query.filter_by(user_id=selected_user)
+
+    if selected_date:
+        query = query.filter_by(date=selected_date)
+
+    reports = query.all()
+
+    data = []
+
+    for report in reports:
+        user = User.query.get(report.user_id)
+        tasks = ReportTask.query.filter_by(report_id=report.id).all()
+
+        for t in tasks:
+            task_name = Task.query.get(t.task_id).name
+
+            data.append({
+                "Employee": user.username,
+                "Date": report.date,
+                "Task": task_name,
+                "Time": t.actual_time,
+                "Notes": t.notes
+            })
+
+    df = pd.DataFrame(data)
+
+    file_path = "report.xlsx"
+    df.to_excel(file_path, index=False)
+
+    return send_file(file_path, as_attachment=True)
+
+
+# ------------------ RUN ------------------
+
+if __name__ == '__main__':
+    app.run()
