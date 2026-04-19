ef8cecf8e3f63b4c4a61e81f5a0172e6fc330704
Corvus Code Review <67117088+CorvusCodeReview@users.noreply.github.com>
2026-04-20 02:03:11 +0530

    Safeguard Logins

--- app.py
+++ app.py
@@ -92,28 +92,6 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/setup_users')
-def setup_users():
-
-    users = [
-        {"username": "employee1", "password": "emp123", "role": "employee"},
-        {"username": "employee2", "password": "emp123", "role": "employee"},
-        {"username": "senior1", "password": "senior123", "role": "senior"},
-        {"username": "manager1", "password": "manager123", "role": "manager"},
-    ]
-
-    for u in users:
-        existing = User.query.filter_by(username=u["username"]).first()
-        if not existing:
-            db.session.add(User(
-                username=u["username"],
-                password=u["password"],
-                role=u["role"]
-            ))
-
-    db.session.commit()
-
-    return "Users created successfully!"
 
 # ------------------ SUBMIT REPORT ------------------
 
